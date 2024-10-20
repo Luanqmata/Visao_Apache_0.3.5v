fbaabdad4966ddee461617bd8259e2b4cb8de64d
Aprendendo C# / Banco de Dados <138558778+Luanqmata@users.noreply.github.com>
2024-10-20 18:03:32 -0300

    ~ 0.3.5 Alpha ~

--- visao_apache.sh
+++ visao_apache.sh
@@ -3,34 +3,38 @@
 GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 RED='\033[0;31m'
-NC='\033[0m' 
+NC='\033[0m'
 
 nome_arquivo=""
 
+pula_linha() {
+    num=$1
+    for ((i=1; i<=num; i++))
+    do
+        echo ""
+    done
+}
 
 logo() {
+    pula_linha 3
     echo -e "${YELLOW}                                   ?  Bem vindo !${NC}" 
-    echo ""
-    echo ""
+    pula_linha 2
     echo -e "${YELLOW}                         ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⣤⣤⣤⣤⣤⣤⣤⣤⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀${NC}"
     echo -e "${YELLOW}                         ⠀⠀⠀⠀⠀⢀⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣄⠀⠀⠀⠀⠀⠀${NC}"
     echo -e "${YELLOW}                         ⠀⠀⠀⣠⣶⣿⣿⡿⣿⣿⣿⡿⠋⠉⠀⠀⠉⠙⢿⣿⣿⡿⣿⣿⣷⣦⡀⠀⠀⠀${NC}"
     echo -e "${YELLOW}                         ⠀⢀⣼⣿⣿⠟⠁⢠⣿⣿⠏⠀⠀⢠⣤⣤⡀⠀⠀⢻⣿⣿⡀⠙⢿⣿⣿⣦⠀⠀${NC}"
-    echo -e "${YELLOW}                         ⣰⣿⣿⡟⠁⠀⠀⢸⣿⣿⠀⠀⠀⢿⣿⣿⡟⠀⠀⠈⣿⣿⡇⠀⠀⠙⣿⣿⣷       ~ v0.3.5 Alpha ~    ${NC}"
+    echo -e "${YELLOW}                         ⣰⣿⣿⡟⠁⠀⠀⢸⣿⣿⠀⠀⠀⢿⣿⣿⡟⠀⠀⠈⣿⣿⡇⠀⠀⠙⣿⣿⣷        ~ v0.3.5 ~ Alpha    ${NC}"
     echo -e "${YELLOW}                         ⠈⠻⣿⣿⣦⣄⠀⠸⣿⣿⣆⠀⠀⠀⠉⠉⠀⠀⠀⣸⣿⣿⠃⢀⣤⣾⣿⣿⠟⠁${NC}"
     echo -e "${YELLOW}                         ⠀⠀⠈⠻⣿⣿⣿⣶⣿⣿⣿⣦⣄⠀⠀⠀⢀⣠⣾⣿⣿⣿⣾⣿⣿⡿⠋⠁⠀⠀${NC}"
     echo -e "${YELLOW}                         ⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠁⠀⠀⠀⠀⠀${NC}"
-    echo -e "${YELLOW}                        ⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠛⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠛⠋⠉⠀⠀⠀⠀⠀⠀${NC}"
-    echo ""
-    echo ""
+    echo -e "${YELLOW}                        ⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠛⠿⠿⠿⠿⠿⠿⠿⠛⠋⠉⠀⠀⠀⠀⠀⠀${NC}"
+    pula_linha 2
     echo -e "${YELLOW}                         - = - = - = visão Apache 2 - = - = - =${NC}"
-    echo ""
+    pula_linha 3
 }
 
 nome_app() {
-    echo ""
-    echo ""
-    echo ""
+    pula_linha 3
     echo -e "${RED}    ▄   ▄█    ▄▄▄▄▄   ██   ████▄     ██   █ ▄▄  ██   ▄█▄     ▄  █ ▄███▄   ${NC}"
     echo -e "${RED}     █  ██   █     ▀▄ █ █  █   █     █ █  █   █ █ █  █▀ ▀▄  █   █ █▀   ▀  ${NC}"
     echo -e "${RED}█     █ ██ ▄  ▀▀▀▀▄   █▄▄█ █   █     █▄▄█ █▀▀▀  █▄▄█ █   ▀  ██▀▀█ ██▄▄    ${NC}"
@@ -38,30 +42,32 @@ nome_app() {
     echo -e "${RED}  █  █   ▐               █              █  █       █ ▀███▀     █  ▀███▀   ${NC}"
     echo -e "${RED}   █▐                   █              █    ▀     █           ▀             ${NC}"
     echo -e "${RED}   ▐                   ▀              ▀          ▀                         ${NC}"
-    echo ""
-    echo ""
+    pula_linha 4
 }
 
 adicionar_arquivo() {
     logo
     echo -e "${GREEN}Obs: (deve estar no mesmo diretório que o script, com .log no final)${NC}"
-    echo ""
+    pula_linha 1
     read -p "Digite o nome do arquivo: " nome_arquivo
-    echo ""
+    pula_linha 1
     echo -e "${GREEN}Adicionando arquivo: $nome_arquivo${NC}"
-    echo ""
+    pula_linha 1
 
     if [ -f "$nome_arquivo" ]; then
         echo -e "${GREEN}Tipo de arquivo: ${NC}"
         head -n 1 "$nome_arquivo"
-        echo ""
+        pula_linha 1
         sleep 2
         echo -e "${GREEN}Arquivo adicionado com sucesso!!${NC}"
-        echo ""
+        pula_linha 1
         sleep 4
         clear
     else
-        echo -e "${GREEN}Arquivo $nome_arquivo não encontrado. Lembre-se do (.log)${NC}"
+	echo -e "${RED}Analisando arquivo ...${NC}"
+	sleep 2
+	pula_linha 1
+        echo -e "${RED}Arquivo $nome_arquivo não encontrado. Lembre-se do (.log)${NC}"
         exit 1
     fi
 }
@@ -78,136 +84,135 @@ exibir_menu() {
     echo "Digite 8. Para Verificar Referencias."
     echo "Digite 9. Para buscar o /etc/passwd."
     echo "Digite 0. Para Sair."
-    echo ""
-    echo ""
+    pula_linha 2
     read -p "Escolha uma opção: " escolha
 
 }
 
 contagem_linhas_arq() {
     echo -e "${RED}"
-    echo ""
+    pula_linha 1
 
     num_linhas=$(wc -l < "$nome_arquivo")
 
     echo "O arquivo '$nome_arquivo' tem $num_linhas linhas."
-    echo ""
+    pula_linha 1
     echo "Tamanho do arquivo: $(du -h "$nome_arquivo" | cut -f1)"
-    echo ""
+    pula_linha 1
     awk '{sum += $10; count++} END {print "Média de tempo de resposta:", sum/count}' "$nome_arquivo"
-    echo ""
+    pula_linha 1
     read -n 1 -s -r -p "Pressione qualquer tecla para continuar..."
     clear
 }
 
 buscar_ips() {
     echo -e "${RED}"
-    echo ""
+    pula_linha 1
     echo "- - - - - - - - - - - IP's - - - - - - - - - - "
-    echo ""
+    pula_linha 1
 
     cat "$nome_arquivo" | cut -d " " -f1 | sort -u
-    echo ""
+    pula_linha 1
     echo "- - - - - - - - - - Requisições de cada IP - - - - - - - - - - - - "
-    echo ""
+    pula_linha 1
     cat "$nome_arquivo" | cut -d " " -f1 | sort | uniq -c | sort -nr
-    echo ""
+    pula_linha 1
     echo "- - - - - - - - - - - Requisições por hora - - - - - - - - - - - -"
-    echo ""
+    pula_linha 1
     awk '{print $4}' "$nome_arquivo" | cut -d: -f2 | sort | uniq -c | sort -nr
-    echo ""
+    pula_linha 1
 
-    echo ""
+    pula_linha 1
     read -n 1 -s -r -p "Pressione qualquer tecla para continuar..."
     clear
 }
 
 distribuicao_codigos_status() {
     echo -e "${RED}"
-    echo ""
+    pula_linha 1
     echo "Distribuição dos códigos de status HTTP:"
-    echo ""
+    pula_linha 1
 
     awk '{print $9}' "$nome_arquivo" | sort | uniq -c | sort -nr
 
-    echo ""
+    pula_linha 1
     read -n 1 -s -r -p "Pressione qualquer tecla para continuar..."
     clear
 }
 
 urls_mais_acessadas() {
     echo -e "${RED}"
-    echo ""
+    pula_linha 1
     echo "URLs mais acessadas:"
-    echo ""
+    pula_linha 1
 
     awk '{print $7}' "$nome_arquivo" | sort | uniq -c | sort -n
 
-    echo ""
+    pula_linha 1
     read -n 1 -s -r -p "Pressione qualquer tecla para continuar..."
     clear
 }
 
 metodos_por_ip() {
     echo -e "${RED}"
-    echo ""
+    pula_linha 1
     echo "Métodos utilizados por endereços IP:"
-    echo ""
+    pula_linha 1
 
     awk '{print $1, $6}' "$nome_arquivo" | sort | uniq -c | sort -nr
 
-    echo ""
+    pula_linha 1
     read -n 1 -s -r -p "Pressione qualquer tecla para continuar..."
     clear
 }
 
 ips_suspeitos() {
     echo -e "${RED}"
-    echo ""
+    pula_linha 1
     echo "IPs com muitas requisições:"
-    echo ""
+    pula_linha 1
 
     awk '{print $1}' "$nome_arquivo" | sort | uniq -c | sort -nr | awk '$1 > 50'
 
-    echo ""
+    pula_linha 1
     read -n 1 -s -r -p "Pressione qualquer tecla para continuar..."
     clear
 }
 
 vizualizador_trafego() {
     echo -e "${RED}"
-    echo ""
+    pula_linha 1
     echo "Visualizador de Tráfego: Distribuição de User-Agents"
-    echo ""
+    pula_linha 1
 
     awk -F\" '{print $6}' "$nome_arquivo" | sort | uniq -c | sort -nr
 
-    echo ""
+    pula_linha 1
     read -n 1 -s -r -p "Pressione qualquer tecla para continuar..."
     clear
 }
 
 verificar_referers() {
     echo -e "${RED}"
-    echo ""
+    pula_linha 1
     echo " Verificar Referências "
-    echo ""
-    
+    pula_linha 1
+
     awk -F\" '{print $4}' "$nome_arquivo" | sort | uniq -c | sort -nr
-    echo ""
+    pula_linha 1
     read -n 1 -s -r -p "Pressione qualquer tecla para continuar..."
     clear
 }
 
 buscar_passwd() {
     echo -e "${RED}"
-    echo ""
+    pula_linha 1
     echo " Buscar informações de usuários no /etc/passwd "
-    echo ""
+    pula_linha 1
 
     awk -F: '{ printf "Usuário: %s\nUID: %s\nGID: %s\nHome: %s\nShell: %s\n\n", $1, $3, $4, $6, $7 }' /etc/passwd
 
-    echo ""
+    pula_linha 1
     read -n 1 -s -r -p "Pressione qualquer tecla para continuar..."
     clear
 }
@@ -236,18 +241,17 @@ main() {
 	elif [ "$escolha" -eq 9 ]; then
             buscar_passwd
 	elif [ "$escolha" -eq 0 ]; then
-	    echo -e "${RED}"
-	    echo ""
+	    echo -e "${YELLOW}"
+	    pula_linha 1
             echo "Saindo do programa..."
-	    echo ""
-            echo "até logo...
-"
+	    pula_linha 1
+            echo "até logo..."
             exit 0
         else
-            echo -e "${RED}"
-	    echo ""
+            echo -e "${YELLOW}"
+	    pula_linha 1
             echo "Por favor digite somente números dentro do escopo do Menu."
-            echo ""
+            pula_linha 1
 	    read -n 1 -s -r -p "Pressione qualqr tecla..."
 	    clear
 	fi
